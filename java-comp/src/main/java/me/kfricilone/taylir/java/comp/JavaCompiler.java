7ef12135465c23fdb5af6ca19fe8df471dc31f31
Kyle Fricilone <kfricilone@gmail.com>
2019-11-17 18:48:48 -0600

    Update copyright

--- java-comp/src/main/java/me/kfricilone/taylir/java/comp/JavaCompiler.java
+++ java-comp/src/main/java/me/kfricilone/taylir/java/comp/JavaCompiler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, Kyle Fricilone
+ * Copyright (c) 2018-2019, Kyle Fricilone <kfricilone@gmail.com>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
